499191ae282a7bba2fbb38642c3bf2aca2313feb
akenz1901 <michael_aka1@icloud.com>
2021-10-27 17:38:05 +0100

    Product controller does allow Update

--- src/main/java/com/ecommerce/shop/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/shop/web/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.ecommerce.shop.web.controller;
 
+import com.ecommerce.shop.data.dto.ProductDto;
 import com.ecommerce.shop.data.model.Product;
 import com.ecommerce.shop.service.ProductService;
+import com.ecommerce.shop.web.controller.exception.ProductNotFoundExistException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.PostUpdate;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -37,4 +41,20 @@ public class ProductController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto){
+
+        log.info("Request id --> {}", id);
+        log.info("Product dto details --> {}", productDto);
+        Product product = null;
+        try{
+            product = productServiceIml.update(id, productDto);
+        }
+        catch(ProductNotFoundExistException ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+        return ResponseEntity.ok().body(product);
+    }
+
 }
