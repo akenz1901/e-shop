cc3ad99f1ccddadd14a2966ba6eb57319d0f37bc
akenz1901 <michael_aka1@icloud.com>
2021-10-27 17:33:21 +0100

    This Class Implemented the Product Service new Method

--- src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
@@ -1,17 +1,26 @@
 package com.ecommerce.shop.service;
 
+import com.ecommerce.shop.data.dto.ProductDto;
 import com.ecommerce.shop.data.model.Product;
 import com.ecommerce.shop.data.repository.ProductRepository;
+import com.ecommerce.shop.service.mapper.ProductMapper;
+import com.ecommerce.shop.web.controller.exception.ProductNotFoundExistException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+@Slf4j
+public class  ProductServiceImpl implements ProductService{
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    ProductMapper productMapper;
 
     @Override
     public Product save(Product product) {
@@ -32,4 +41,21 @@ public class ProductServiceImpl implements ProductService{
     public void deleteById(Long id) {
         productRepository.deleteById(id);
     }
+
+    @Override
+    public Product update(Long id, ProductDto productDto) throws ProductNotFoundExistException {
+        if(productDto==null){
+            throw new NullPointerException("Product Dto cannot be null");
+        }
+        Optional <Product> result = productRepository.findById(id);
+        if(result.isPresent()){
+            Product product = result.get();
+            productMapper.mapDtoToProduct(productDto, product);
+            return productRepository.save(product);
+        }
+        else {
+            throw new ProductNotFoundExistException("Product with id " + id + "does not exist");
+        }
+    }
+
 }
