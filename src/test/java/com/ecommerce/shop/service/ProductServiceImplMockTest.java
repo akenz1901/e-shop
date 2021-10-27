7141f7af178fed3de5f177ab3841405d421a2731
akenz1901 <michael_aka1@icloud.com>
2021-10-27 17:46:57 +0100

    Test severred which consists of Product mock test and Real Test

--- src/test/java/com/ecommerce/shop/service/ProductServiceImplMockTest.java
+++ src/test/java/com/ecommerce/shop/service/ProductServiceImplMockTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
-class ProductServiceImplTest {
+class ProductServiceImplMockTest {
 
     @Mock
     ProductRepository productRepository;
--- src/test/java/com/ecommerce/shop/service/ProductServiceImplTest.java
+++ src/test/java/com/ecommerce/shop/service/ProductServiceImplTest.java
@@ -1,2 +1,40 @@
-package com.ecommerce.shop.service;public class ProductServiceImplTest {
+package com.ecommerce.shop.service;
+
+import com.ecommerce.shop.data.dto.ProductDto;
+import com.ecommerce.shop.data.model.Product;
+import com.ecommerce.shop.data.repository.ProductRepository;
+import com.ecommerce.shop.web.controller.exception.ProductNotFoundExistException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Sql(scripts = {"/db/insert.sql"})
+public class ProductServiceImplTest {
+
+
+    @Autowired
+    ProductService productServiceImpl;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void updateProductTest() throws ProductNotFoundExistException {
+        Product product = productRepository.findById(110L).orElse(null);
+
+        assertThat(product).isNotNull();
+        assertThat(product.getName()).isEqualTo("luxury");
+
+        ProductDto productDto = new ProductDto();
+        productDto.setName("Mexican chair");
+
+        product = productServiceImpl.update(110L, productDto);
+        assertThat(product.getName()).isEqualTo(productDto.getName());
+
+
+    }
 }
